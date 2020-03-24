c67dcaf2e36d9735f1c771d0bbb987b6b48b0db1
Ginda Chen <>
2020-03-24 14:29:32 -0500

    Add tests to config

--- tests/basic.py
+++ tests/basic.py
@@ -7,5 +7,15 @@ def test_config():
     e = c.from_dict(d)
     assert c == e
 
+def test_push_pop():
+    c = Config()
+    c.push(worker_port="49000")
+    print(c)
+    c.pop()
+    print(c)
 
-test_config()
+
+
+
+# test_config()
+test_push_pop()
